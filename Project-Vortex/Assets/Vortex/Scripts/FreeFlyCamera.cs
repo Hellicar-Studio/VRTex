cb797cfe4fc4a27e9ea79d7f3db55cfed9cb8a07
Bentleyj <jgbentley10@gmail.com>
2017-06-05 16:40:55 +0100

    Added Flythrough camera for documentation

---
+++ Project-Vortex/Assets/Vortex/Scripts/FreeFlyCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeFlyCamera : MonoBehaviour {
+
+    public float lookSpeed = 15.0f;
+    public float moveSpeed = 15.0f;
+
+    float rotationX = 0.0f;
+    float rotationY = 0.0f;
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Cursor.visible = false;
+
+        rotationX += Input.GetAxis("Mouse X") * lookSpeed;
+        rotationY += Input.GetAxis("Mouse Y") * lookSpeed;
+        rotationY = Mathf.Clamp(rotationY, -90, 90);
+
+        transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+        transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+
+        transform.position += transform.forward * moveSpeed * Input.GetAxis("Vertical");
+        transform.position += transform.right * moveSpeed * Input.GetAxis("Horizontal");
+    }
+}

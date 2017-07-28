6442961528e7570b9db86a904b19c3c714d4f10d
Bentleyj <jgbentley10@gmail.com>
2017-07-28 14:30:42 +0100

    Worked out more glow stuff with the standard shader. Also added scene for jim to play in.

--- Project-Vortex/Assets/VortexGlow/Scripts/LightGun.cs
+++ Project-Vortex/Assets/VortexGlow/Scripts/LightGun.cs
@@ -3,24 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LightGun : MonoBehaviour {
-    public GameObject l;
-    private Material mat;
+    public GameObject bullet;
     private Transform t;
-    public LightManager lightManager;
     private Rigidbody bulletBody;
+    private Light bulletLight;
+    private Material orbMat;
+
+    private List<Coroutine> activeRoutines;
+
+    public float maxRange;
+
 	// Use this for initialization
 	void Start () {
         t = this.transform;
-        l = FindObjectOfType<LightBullet>().gameObject;
-        lightManager = FindObjectOfType<LightManager>();
-        mat = l.GetComponent<MeshRenderer>().material;
-        bulletBody = l.GetComponent<Rigidbody>();
+        bullet = Instantiate(bullet, t);
+        bullet.transform.parent = t.parent;
+        bulletBody = bullet.GetComponent<Rigidbody>();
+        bulletLight = bullet.GetComponent<Light>();
+        //orbMat = bullet.GetComponentInChildren<MeshRenderer>().material;
+
+        activeRoutines = new List<Coroutine>();
+
         bulletBody.interpolation = RigidbodyInterpolation.Interpolate;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetKeyUp("p"))
+        if(Input.GetKeyUp("space"))
         {
             fireLight();
         }
@@ -28,19 +37,57 @@ public class LightGun : MonoBehaviour {
 
     void fireLight()
     {
-        l.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
-        StartCoroutine(Fade(new Color(1.0f, 1.0f, 1.0f)));
-        bulletBody.velocity = transform.forward;
+        for(int i = 0; i < activeRoutines.Count; i++)
+        {
+            if(activeRoutines[i] != null)
+            {
+                StopCoroutine(activeRoutines[i]);
+            }
+        }
+        activeRoutines.Clear();
+        bullet.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+        if(orbMat != null)
+        {
+            activeRoutines.Add(StartCoroutine(FadeOrb(new Color(1.0f, 1.0f, 1.0f))));
+        }
+        activeRoutines.Add(StartCoroutine(FadeLight(50.0f)));
+        activeRoutines.Add(StartCoroutine(MoveForward(t.forward)));
     }
 
-    IEnumerator Fade(Color target)
+    IEnumerator FadeOrb(Color target)
     {
-        Color col = mat.GetColor("_EmissionColor");
+        Color col = orbMat.GetColor("_RimColor");
         while(col != target)
         {
             col = Color.Lerp(col, target, 0.1f);
-            mat.SetColor("_EmissionColor", col);
+            orbMat.SetColor("_RimColor", col);
             yield return null;
         }
     }
+
+    IEnumerator FadeLight(float target)
+    {
+        float range = bulletLight.range;
+        while (range < target)
+        {
+            range = Mathf.Lerp(range, target, 0.1f);
+            bulletLight.range = range;
+            yield return null;
+        }
+    }
+
+    IEnumerator MoveForward(Vector3 forward)
+    {
+        bullet.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+        Vector3 pos = bullet.transform.position;
+        float dist = Vector3.Distance(pos, t.position);
+        while (dist < maxRange)
+        {
+            pos = Vector3.Lerp(pos, pos + forward * maxRange, 0.01f);
+            bulletLight.transform.position = pos;
+            dist = Vector3.Distance(pos, t.position);
+            yield return null;
+        }
+    }
+
 }
